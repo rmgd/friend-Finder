9fb78dd872c0592dccf6e292010f6fc5f4b1f308
rmgd <randall.dow24@gmail.com>
2018-09-18 22:12:08 -0400

    added assignment files to git

---
+++ app/data/friends.js
@@ -0,0 +1,64 @@
+var friends = [
+  {
+    name: 'Jason',
+    photo:
+      'https://frolic.media/wp-content/uploads/2018/05/jasonmomoa_lead-864x482.jpg',
+    scores: ['5', '1', '4', '4', '3', '2', '1', '3', '5', '2']
+  },
+  {
+    name: 'Idris',
+    photo:
+      'https://assets.forwardcdn.com/images/cropped/idris-elba-1441132869.jpg',
+    scores: ['4', '2', '3', '1', '4', '3', '2', '4', '1', '5']
+  },
+  {
+    name: 'RiRi',
+    photo:
+      'https://28a0ps69xt73635su3zdf8y1-wpengine.netdna-ssl.com/wp-content/uploads/2017/11/rih.jpg',
+    scores: ['3', '1', '4', '2', '3', '4', '1', '5', '2', '1']
+  },
+  {
+    name: 'Tyra',
+    photo:
+      'https://www.ebony.com/wp-content/uploads/2018/01/antm-24-final-603x339.jpg',
+    scores: ['2', '3', '5', '1', '2', '5', '5', '4', '3', '2']
+  },
+  {
+    name: 'Henry',
+    photo:
+      'https://m.media-amazon.com/images/M/MV5BMTUxNTExMzUzOF5BMl5BanBnXkFtZTgwOTI1MjA3OTE@._V1_.jpg',
+    scores: ['1', '4', '1', '5', '3', '1', '5', '3', '1', '3']
+  },
+  {
+    name: 'Cardi',
+    photo:
+      'https://lph5i1b6c053kq7us26bdk75-wpengine.netdna-ssl.com/wp-content/uploads/2018/06/cardi-b-facts.jpg',
+    scores: ['5', '3', '5', '1', '4', '2', '4', '2', '5', '4']
+  },
+  {
+    name: 'Blake',
+    photo:
+      'https://m.media-amazon.com/images/M/MV5BMTU2Njk0NTUyNl5BMl5BanBnXkFtZTcwODE0OTgyNw@@._V1_SY1000_CR0,0,757,1000_AL_.jpg',
+    scores: ['1', '2', '1', '5', '4', '3', '4', '3', '4', '4']
+  },
+  {
+    name: 'Adam',
+    photo:
+      'https://lux.fm/uploads/800x450_DIR/importedNews/main/2016/09/6e720398a59f276929e5bee079e1ccb7aaa1d0d2.jpg',
+    scores: ['2', '4', '5', '4', '3', '2', '3', '1', '1', '3']
+  },
+  {
+    name: 'Dwayne',
+    photo:
+      'https://asisucedeleon.mx/wp-content/uploads/2018/04/a1a345ffgx6C3Hq77KQ3c690.jpg',
+    scores: ['3', '5', '4', '2', '1', '4', '5', '2', '5', '4']
+  },
+  {
+    name: 'Taraji',
+    photo:
+      'http://blackvoicenews.com/wp-content/uploads/2017/08/Taraji-P.-Henson.jpeg',
+    scores: ['4', '1', '5', '2', '3', '4', '5', '1', '5', '3']
+  }
+];
+
+module.exports = friends;
